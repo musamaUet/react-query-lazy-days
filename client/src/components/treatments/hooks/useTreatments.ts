9498ecd33f7d8868329dc1ab0bf00b757dd26362
Furqan Aziz <furqan.dev6@gmail.com>
2022-06-22 16:04:52 +0500

    queryClient errorHandler

--- client/src/components/treatments/hooks/useTreatments.ts
+++ client/src/components/treatments/hooks/useTreatments.ts
@@ -6,7 +6,7 @@ import { useCustomToast } from '../../app/hooks/useCustomToast';
 
 // for when we need a query function for useQuery
 async function getTreatments(): Promise<Treatment[]> {
-	const { data } = await axiosInstance.get('/treatments');
+	const { data } = await axiosInstance.get('/treatmentsa');
 	return data;
 }
 
@@ -14,15 +14,7 @@ export function useTreatments(): Treatment[] {
 	const toast = useCustomToast();
 
 	const fallback = [];
-	const { data = fallback } = useQuery(queryKeys.treatments, getTreatments, {
-		onError: (error) => {
-			const title =
-				error instanceof Error
-					? error.toString().replace(/^Error:\s*/, '')
-					: 'error connecting to server';
-			toast({ title, status: 'error' });
-		},
-	});
+	const { data = fallback } = useQuery(queryKeys.treatments, getTreatments);
 	console.log('data', data);
 	return data;
 }
